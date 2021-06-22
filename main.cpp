4aa5980f2019c513bd7b853756f50eef90112575
farshid616 <farshid.616@gmail.com>
2021-06-22 09:50:19 +0300

    Added main body
    Developed main
     - Added loop to receive sentence from input and return intent

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include "SentenceParser.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Please enter your sentence or q for exit:" << std::endl;
+    std::string input;
+    //Loop to receiving input and response after detecting intent of input sentence
+    while (true) {
+        getline(std::cin, input);
+        //Exit when the user inputs q
+        if(!std::cin || input == "q" || input == "Q")
+            break;
+        std::cout<<"Intent: "<<SentenceParser::GetInstance()->GetIntent(input)<<std::endl;
+        std::cout << "Please enter your sentence or q for exit:" << std::endl;
+    }
     return 0;
 }
